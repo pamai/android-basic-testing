a59aa0bca904a7210c89e15bc41aab9268a7fe82
pamai <pamai_5656@hotmail.com>
2017-11-15 15:45:39 +0700

    Add Correct Testcase

--- app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
+++ app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
@@ -40,4 +40,13 @@ public class MainActivityTest {
         onView(withId(R.id.emailInput)).check(matches(withText("")));
     }
 
+    @Test
+    public void correctTest(){
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.revertButton)).perform(scrollTo(), click());
+        onView(withId(R.id.userNameInput)).perform(scrollTo() ,replaceText("Paniti"));
+        onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText("pamai_5656@hotmail.com"));
+        onView(withId(R.id.saveButton)).perform(scrollTo(), click());
+    }
+
 }
